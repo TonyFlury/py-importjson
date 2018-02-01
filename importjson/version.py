fc123dab95da8b4585b03b0fc10ae5752bc21a5c
Tony Flury <anthony.flury@btinternet.com>
2018-02-01 13:01:56 +0000

    Release of Issue 11 change - version 0.1.1

--- importjson/version.py
+++ importjson/version.py
@@ -2,7 +2,7 @@
 """ Common version number - single source version """
 
 __version__ = '0.1.1'
-__release__ = '0.1.1.a3'
+__release__ = '0.1.1'
 __copyright__ = u'© 2015-2018 : Tony Flury'
 __author__ = u'Tony Flury'
 __email__ = u'anthony.flury@btinternet.com'
